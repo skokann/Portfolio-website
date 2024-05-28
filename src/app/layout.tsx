a9fd15af456d8868199a88968778fe6470080810
Jiří Zubryckyj <zubryckyj@icloud.com>
2024-05-28 21:55:28 +0200

    added showing img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-                <link rel="shortcut icon" href="/icon.ico" />
+          <link rel="icon" href="/path/to/favicon.ico" type="image/x-icon"/>
+          <meta property="og:title" content="JIRI DEV" />
+          <meta property="og:description" content="JIRI DEV" />
+          <meta property="og:image" />
+          <meta property="og:url"/>
+          <link rel="shortcut icon" href="/icon.ico" />
       <body className={`${fira_Code.className} bg-[#0A0E1B] text-slate-100 mx-auto p-2 `}>{children}</body>
     </html>
   );
